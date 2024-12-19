6d9769db9f7d358b75cf6c4b008a33e08ee465d5
Aryan C <aryanchaudhary1900@gmail.com>
2024-12-19 21:36:24 +0530

    UI for AddExpense.ky added

--- app/src/main/java/com/example/avengers_tracker/AddExpense.kt
+++ app/src/main/java/com/example/avengers_tracker/AddExpense.kt
@@ -2,19 +2,21 @@ package com.example.avengers_tracker
 
 import android.text.Layout
 import android.widget.GridLayout
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+//import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -48,7 +50,11 @@ fun AddExpense() {
                     end.linkTo(parent.end)
 
                 }) {
-
+                Image(
+                    painter = painterResource(id = R.drawable.ic_back),
+                    contentDescription = null,
+                    modifier = Modifier.align(Alignment.CenterStart)
+                )
                 Text(
                     text = "Add Expense",
                     fontSize = 20.sp,
@@ -68,11 +74,14 @@ fun AddExpense() {
 
 
             }
-            DataForm(modifier = Modifier.constraintAs(card) {
-                top.linkTo(nameRow.bottom)
-                start.linkTo(parent.start)
-                end.linkTo(parent.end)
-            })
+
+            DataForm(modifier = Modifier
+                .padding(top = 60.dp)
+                .constrainAs(card) {
+                    top.linkTo(nameRow.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                })
         }
     }
 }
@@ -83,30 +92,120 @@ fun DataForm(modifier: Modifier) {
         modifier = modifier
             .padding(16.dp)
             .fillMaxWidth()
+            .shadow(8.dp)
             .clip(RoundedCornerShape(16.dp))
-            .padding(16.dp)
             .background(Color.White)
-            .height(10.dp)
+            .padding(16.dp)
+            .verticalScroll(rememberScrollState())
+
+
     ) {
-//        Text(
-//            text = "Add Expense",
-//            fontSize = 20.sp,
-//            color = Color.White,
-//            fontWeight = FontWeight.Bold,
-//            modifier = Modifier
-//                .padding(16.dp)
-//
-//
-//        )
-//
-//        Image(
-//            painter = painterResource(id = R.drawable.dots_menu),
-//            contentDescription = null,
-//            modifier = Modifier.align(Alignment.CenterEnd)
-//        )
+        Text(
+            text = "Type",
+            fontSize = 17.sp,
+
+            )
+        OutlinedTextField(
+            value = "",
+            onValueChange = { },
+            label = "Enter Type",
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = "Name",
+            fontSize = 17.sp,
+
+            )
+        OutlinedTextField(
+            value = "",
+            onValueChange = { },
+            label = "Enter Name",
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = "Category",
+            fontSize = 17.sp,
+
+            )
+        OutlinedTextField(
+            value = "",
+            onValueChange = { },
+            label = "Enter Category",
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = "Amount",
+            fontSize = 17.sp,
+
+            )
+        OutlinedTextField(
+            value = "",
+            onValueChange = { },
+            label = "Enter Amount",
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = "Date",
+            fontSize = 17.sp,
+
+            )
+        OutlinedTextField(
+            value = "",
+            onValueChange = { },
+            label = "Enter Name",
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = {}, modifier = Modifier
+                .clip(RoundedCornerShape(8.dp))
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = "Add Expense",
+                color = Color.White,
+                fontSize = 14.sp,
+
+                )
+
+        }
     }
 }
 
+
+@Composable
+fun OutlinedTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    label: String,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .border(1.dp, Color.Gray)
+            .padding(8.dp)
+            .fillMaxWidth()
+    ) {
+        BasicTextField(
+            value = value,
+            onValueChange = onValueChange,
+            singleLine = true,
+            decorationBox = { innerTextField ->
+                if (value.isEmpty()) {
+                    Text(text = label, color = Color.Gray)
+                }
+                innerTextField()
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
 
 @Composable
 @Preview(showBackground = true)
